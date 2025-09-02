8140102bc79594adfd56edf9d8e827437e8489fe
Sascha Brandt <sascha.brandt@hva.nl>
2025-09-02 16:43:46 +0200

    Harden validation and fix nextjs config

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 
 Route::middleware('web')->group(function () {
     Route::post('/login', [AuthController::class, 'login']);
