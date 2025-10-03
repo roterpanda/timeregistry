3d3dea639ba02769e8c7032855d57dea1a003433
Sascha Brandt <sascha.brandt@hva.nl>
2025-10-03 17:32:17 +0200

    Add additional check to make sure users cannot write time to private projects

--- backend/app/Http/Controllers/TimeRegistrationController.php
+++ backend/app/Http/Controllers/TimeRegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -23,7 +24,7 @@ class TimeRegistrationController extends Controller
     {
         $user = Auth::guard('web')->user();
         if (!$user) {
-            return response()->json('Unauthorized', 401);
+            return response()->json('Unauthenticated', 401);
         }
         $validator = Validator::make($request->all(), [
             'project_id' => 'required|exists:projects,id',
@@ -32,6 +33,11 @@ class TimeRegistrationController extends Controller
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
+        }
+
+        $project = Project::find($request->project_id);
+        if (!$project || ($project->owner_id !== $user->id && !$project->is_public)) {
+            return response()->json('Unauthorized', 403);
         }
 
         $timeRegistration = $user->timeRegistrations()->create($request->all());
--- backend/tests/Feature/TimeRegistrationControllerTest.php
+++ backend/tests/Feature/TimeRegistrationControllerTest.php
@@ -13,30 +13,44 @@ class TimeRegistrationControllerTest extends TestCase
     use RefreshDatabase;
 
     /** @var User an authenticated user */
-    private User $user;
+    private User $user1;
 
-    /** @var Project a project */
-    private Project $project;
+    /** @var User another authenticated user */
+    private User $user2;
+
+    /** @var Project a public project */
+    private Project $publicProject;
+
+    /** @var Project a private project */
+    private Project $privateProject;
 
     public function setUp(): void
     {
         parent::setUp();
-        $this->user = User::factory()->create();
-        $this->project = Project::create([
+        $this->user1 = User::factory()->create();
+        $this->privateProject = Project::create([
             'name' => 'Test Project',
             'description' => 'This is a test project',
-            'is_public' => true,
-            'owner_id' => $this->user->id,]
+            'is_public' => false,
+            'owner_id' => $this->user1->id,]
         );
-        $this->project->save();
+        $this->privateProject->save();
+
+        $this->user2 = User::factory()->create();
+        $this->publicProject = Project::create([
+                'name' => 'Test Project Public',
+                'description' => 'This is a test project for user 2',
+                'is_public' => true,
+                'owner_id' => $this->user2->id,]
+        );
 
     }
 
     public function test_create_timeregistration_with_basic_fields(): void
     {
-        $this->actingAs($this->user, 'web');
+        $this->actingAs($this->user1, 'web');
         $response = $this->postJson('/api/v1/timeregistration', [
-            'project_id' => $this->project->id,
+            'project_id' => $this->privateProject->id,
             'date' => '2024-01-01',
             'duration' => 8,
             'kilometers' => 1000,
@@ -44,12 +58,45 @@ class TimeRegistrationControllerTest extends TestCase
         ]);
         $response->assertStatus(201);
         $this->assertDatabaseHas('time_registrations', [
-            'project_id' => $this->project->id,
+            'project_id' => $this->privateProject->id,
             'date' => '2024-01-01',
             'duration' => 8,
             'kilometers' => 1000,
             'notes' => 'Test notes'
         ]);
+        $response = $this->postJson('/api/v1/timeregistration', [
+            'project_id' => $this->publicProject->id,
+            'date' => '2024-01-01',
+            'duration' => 8,
+        ]);
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('time_registrations', [
+            'project_id' => $this->publicProject->id,
+            'date' => '2024-01-01',
+            'duration' => 8,
+        ]);
+    }
+
+    public function test_create_timeregistration_with_invalid_project_id(): void
+    {
+        $this->actingAs($this->user1, 'web');
+        $response = $this->postJson('/api/v1/timeregistration', [
+            'project_id' => 9999,
+            'date' => '2024-01-01',
+            'duration' => 8,
+        ]);
+        $response->assertStatus(422);
+    }
+
+    public function test_create_timeregistration_not_authorized(): void
+    {
+        $this->actingAs($this->user2, 'web');
+        $response = $this->postJson('/api/v1/timeregistration', [
+            'project_id' => $this->privateProject->id,
+            'date' => '2024-01-01',
+            'duration' => 8,
+        ]);
+        $response->assertStatus(403);
     }
 
 
