cc7ea5354437997c0650c7c5ecddbeaf72ff86f3
Sascha Brandt <sascha.brandt@hva.nl>
2025-12-07 16:47:26 +0100

    Fix quote error in timeregistration/list/page.tsx

--- frontend-web/app/dashboard/timeregistration/list/page.tsx
+++ frontend-web/app/dashboard/timeregistration/list/page.tsx
@@ -168,7 +168,7 @@ export default function TimeRegistrationTablePage() {
               });
             const blob = new Blob([csvResponse.data], {type: "text/csv;charset=utf-8"});
             const url = URL.createObjectURL(blob);
-            const a = document.createElement('a');
+            const a = document.createElement("a");
             a.href = url;
             a.download = "timeregistrations.csv";
             a.click();
